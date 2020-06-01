6ffbb9e8b6b24051d51616f226b47ba535c9c464
pcNewbee <m13117328229@163.com>
2020-06-01 15:36:32 +0800

    testGit

---
+++ src/main/java/com/pc/hellogit/HelloGit.java
@@ -0,0 +1,13 @@
+package com.pc.hellogit;
+
+/**
+ * @author pc
+ * @date 2020/6/1 12:33
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hell pc");
+        System.out.println("hell pc1");
+    }
+}
